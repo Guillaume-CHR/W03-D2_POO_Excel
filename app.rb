4640188db8adf5c96bda4d6ed29578e72cba8105
guillaume-CHR <guillaume.christe@gmail.com>
2019-01-22 10:38:30 +0100

    Initiation of GitHub

---
+++ app.rb
@@ -0,0 +1,26 @@
+#! /usr/bin/env ruby
+##****************************************************************************
+# RUBY - @NameOfTheProgram
+#****************************************************************************
+#   Ruby's Program - Name of the program
+#   Written by: Guillaume CHRISTE
+# 	Date: Date of modification
+#   
+#   Description:
+# 		- 
+# 		- 
+# 		
+# 	Gems:
+		require 'bundler'
+		Bundler.require
+
+#	Links:
+		$:.unshift File.expand_path("./../lib", __FILE__)
+		require 'app/scrapper'
+		require 'views/'
+#
+#****************************************************************************
+
+# End of Program's name
+#.............................................................................
+#.............................................................................
