fc639f2ed7e02b7f5ed8788c8508872f4c32774f
guillaume-CHR <guillaume.christe@gmail.com>
2019-01-22 17:33:25 +0100

    Final upload

--- app.rb
+++ app.rb
@@ -37,7 +37,20 @@ def save_as_JSON
 end
 
 def save_as_spreedsheet
-  
+  session = GoogleDrive::Session.from_config("config.json")
+  ws = session.spreadsheet_by_key("1Gc83X5sHlXEhKl4EZar1qdVqsJCpLC14ZrnQZAh9JO0").worksheets[0]
+
+  # Dumps all cells.
+  i = 1
+  main.each do |_hash|
+    _hash.each do |x,y|
+      ws[i,1] = x
+      ws[i,2] = y
+      i+=1
+    end
+  end
+  ws.save
+  ws.reload
 end
 
 def save_as_CSV
@@ -45,18 +58,45 @@ def save_as_CSV
 end
 
 def perform
-  #Implement user selection
-  save_as_JSON
+  system("clear")
+  puts "***************************************************************************"
+  puts " Hey, user!! Please select a format to extract your list of array"
+  puts "***************************************************************************"
+  puts ""
+  puts "Select 1 for JSON"
+  puts "Enter 2 for Google Sheets"
+  puts "Enter 3 for CSV"
+  puts ""
+  puts "***************************************************************************"
+  print "Enter your choice here > "
+  choice = gets.chomp.to_i
 
-  save_as_CSV
+  if choice != choice.to_i or choice > 3 or choice <=0
+    puts "ERROR - Let's start over in 1 sec"
+    sleep(1)
+    perform
+  elsif choice == 1
+    puts "_nYou've selected format JSON - Great choice for unknown use"
+    puts "(... loading...)"
+    save_as_JSON
+    system("cd db/email.JSON")
+    puts "(All set)"
+  elsif choice == 2
+    puts "You've selected format Google Drive - Great choice to save the planet in Cloud"
+    puts "(... loading...)"
+    save_as_spreedsheet
+    puts "Access the hereafter link for checks: https://docs.google.com/spreadsheets/d/1Gc83X5sHlXEhKl4EZar1qdVqsJCpLC14ZrnQZAh9JO0/edit#gid=0"
+    puts "(All set)"
+  elsif choice == 3
+    puts "You've selected format CSV - Great choice, all you need is to convert it in Excel"
+    puts "(... loading...)"
+    save_as_CSV
+    system("cd db/email.csv")
+    puts "(All set)"
+  end
 end
 
 perform
-
-##### Google Drive
-
-##### CSV
-
 # End of Program's name
 #.............................................................................
 #.............................................................................
