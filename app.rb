62fc3a9018b00f3a030422fdd91c9430cba6f645
guillaume-CHR <guillaume.christe@gmail.com>
2019-01-22 18:02:16 +0100

    New upload

--- app.rb
+++ app.rb
@@ -1,18 +1,18 @@
 #! /usr/bin/env ruby
 ##****************************************************************************
-# RUBY - @NameOfTheProgram
+# RUBY - Main Application 
 #****************************************************************************
-#   Ruby's Program - Name of the program
+#   Ruby's Program - app
 #   Written by: Guillaume CHRISTE
 # 	Date: Date of modification
 #   
 #   Description:
-# 		- 
-# 		- 
+# 		- Calls the method 'scrapper' to retrieve an array of city/emails
+# 		- Convert this array into either JSON, Google Sheet or CSV
 # 		
 # 	Gems:
-		require 'bundler'
-		Bundler.require
+  		require 'bundler'
+  		Bundler.require
 
 #	Links:
 		$:.unshift File.expand_path("./../lib", __FILE__)
@@ -20,28 +20,28 @@
 		# require 'views/'
 #
 #****************************************************************************
-def main
+def main # Create an instance of class 'Scrapper' with attribute array_town
   url = "http://annuaire-des-mairies.com/val-d-oise.html"
   scrapper = Scrapper.new(url)
   array_to_record = scrapper.array_town
 end
 
-def record(file)
+def record(file) #For each sub-hash, the value is join with commas
   main.each do |_hash|
   	file.puts(_hash.map {|x,y| "#{x},#{y}"}.join(''))
   end
 end
 
-def save_as_JSON
+def save_as_JSON #Call method 'record' with file 'email.JSON'
   record(File.open("db/email.JSON","w"))
 end
 
-def save_as_spreedsheet
+def save_as_spreedsheet #Use Google Drive API to save in Google Drive
   session = GoogleDrive::Session.from_config("config.json")
   ws = session.spreadsheet_by_key("1Gc83X5sHlXEhKl4EZar1qdVqsJCpLC14ZrnQZAh9JO0").worksheets[0]
 
-  # Dumps all cells.
   i = 1
+  #On each sub-hash, the key(x) and value(y) are recorded in a dedicated Google Drive cell
   main.each do |_hash|
     _hash.each do |x,y|
       ws[i,1] = x
@@ -53,19 +53,19 @@ def save_as_spreedsheet
   ws.reload
 end
 
-def save_as_CSV
+def save_as_CSV #Call method 'record' with file 'email.csv'
   record(File.open("db/email.csv","w"))
 end
 
-def perform
+def perform #User menu
   system("clear")
   puts "***************************************************************************"
   puts " Hey, user!! Please select a format to extract your list of array"
   puts "***************************************************************************"
   puts ""
-  puts "Select 1 for JSON"
-  puts "Enter 2 for Google Sheets"
-  puts "Enter 3 for CSV"
+  puts " Enter 1 for JSON"
+  puts " Enter 2 for Google Sheets"
+  puts " Enter 3 for CSV"
   puts ""
   puts "***************************************************************************"
   print "Enter your choice here > "
@@ -85,7 +85,7 @@ def perform
     puts "You've selected format Google Drive - Great choice to save the planet in Cloud"
     puts "(... loading...)"
     save_as_spreedsheet
-    puts "Access the hereafter link for checks: https://docs.google.com/spreadsheets/d/1Gc83X5sHlXEhKl4EZar1qdVqsJCpLC14ZrnQZAh9JO0/edit#gid=0"
+    puts "Access the hereafter link for checks: https://docs.google.com/spreadsheets/d/1Gc83X5sHlXEhKl4EZar1qdVqsJCpLC14ZrnQZAh9JO0/edit?usp=sharing"
     puts "(All set)"
   elsif choice == 3
     puts "You've selected format CSV - Great choice, all you need is to convert it in Excel"
