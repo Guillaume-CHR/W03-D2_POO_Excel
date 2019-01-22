db1aa7de90079a563f8ce8f6e42af82a9bc7a2fc
guillaume-CHR <guillaume.christe@gmail.com>
2019-01-22 12:42:01 +0100

    Upload JSON & CSV

--- app.rb
+++ app.rb
@@ -17,9 +17,39 @@
 #	Links:
 		$:.unshift File.expand_path("./../lib", __FILE__)
 		require 'app/scrapper'
-		require 'views/'
+		# require 'views/'
 #
 #****************************************************************************
+def main
+  url = "http://annuaire-des-mairies.com/val-d-oise.html"
+  scrapper = Scrapper.new(url)
+  array_to_record = scrapper.array_town
+end
+
+def save_as_JSON
+  json_file = File.open("db/email.JSON","w")
+  i=1
+  main.each do |_hash|
+  	json_file.puts(_hash.map {|x,y| "#{i},#{x},#{y}"}.join(''))
+  	i+=1
+  end
+end
+
+def save_as_CSV
+	system("cp db/email.JSON db/email.csv")
+end
+
+def perform
+  #Implement user selection
+  save_as_JSON
+  save_as_CSV
+end
+
+perform
+
+##### Google Drive
+
+##### CSV
 
 # End of Program's name
 #.............................................................................
--- lib/app/scrapper.rb
+++ lib/app/scrapper.rb
@@ -1,6 +1,6 @@
 #! /usr/bin/env ruby
 ##****************************************************************************
-# RUBY - @NameOfTheProgram
+# RUBY - Scrapper
 #****************************************************************************
 #   Ruby's Program - Name of the program
 #   Written by: Guillaume CHRISTE
@@ -15,8 +15,27 @@
 		Bundler.require
 #
 #****************************************************************************
+class Scrapper
+	attr_accessor :array_town
 
+	def initialize(url)
+		url_dept = url
+	    html = Nokogiri::HTML(open(url_dept))
+	    @array_town = []
 
+	    html.css('.lientxt').map do |link_to_town|
+	      _town = link_to_town.text
+	      _email = get_townhall_email("http://annuaire-des-mairies.com#{(link_to_town['href'])[1..(link_to_town['href']).length]}")
+	      @array_town << {_town => _email}
+	    end
+	    @array_town.flatten!
+	end
+
+	def get_townhall_email(url) #From an url retrieves the email address 
+	    html = Nokogiri::HTML(open(url))
+	    email_town = html.css('section:nth-child(2) div table tbody tr:nth-child(4) > td:nth-child(2)').text
+	end
+end
 # End of Program's name
 #.............................................................................
 #.............................................................................
